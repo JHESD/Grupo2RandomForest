dfa41b77b438fac8aadcd995ab58173a04207e32
FlaviaAndreaNogalesS <fandreans@gmail.com>
2024-06-05 23:01:05 -0400

    comit de flavia

--- Main.py
+++ Main.py
@@ -9,3 +9,12 @@ data = pd.read_csv('CreditoPersonal.csv')
 # Suponiendo que la columna objetivo se llama 'target' y las características son todas las demás columnas
 X = data.drop('target', axis=1)  # Características
 y = data['target']  # Etiqueta
+
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Crear el modelo de Random Forest
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Entrenar el modelo
+clf.fit(X_train, y_train)
